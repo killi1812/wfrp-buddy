2e022effda921028aa2ff9f36f75b1a40f6edca3
killi1812 <cvok.fran@gmail.com>
2026-03-23 21:43:20 +0100

    Added character model and connected models

---
+++ wfrp-buddy/model/caracter.go
@@ -0,0 +1,239 @@
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type Caracter struct {
+	CaracterId uuid.UUID
+	Name       string
+	Species    Species
+	Class      Class
+
+	Career Career
+
+	Status Status
+
+	Description Description
+
+	Talents        []Talent
+	Characteristic Characteristic
+	Movment        Movment
+	Ambitions      Ambitions
+
+	Party *Party
+
+	Armour      Armour
+	Trappings   []Trapping
+	Encumbrance Encumbrance
+	Welth       Welth
+	Wounds      Wounds
+	Weapons     []Weapon
+	Spells      []Spell
+	Prayers     []Prayer
+
+	Sin        int
+	Corruption Corruption
+	Psychology Psychology
+
+	Points Points
+}
+
+type (
+	// TODO: implement species enum
+	Species string
+	// TODO: implement class enum
+	Class string
+	// TODO: implement BookVersion enum
+	BookVersion string
+	// TODO: implement StatusTier enum
+	StatusTier string
+	// TODO: implement Location enum
+	Location string
+	// TODO: implement WeaponGroup enum
+	WeaponGroup string
+)
+
+type Talent struct {
+	Id          uuid.UUID
+	Name        string
+	Version     BookVersion
+	MaxLvl      int
+	Test        string `json:"test,omitempty"`
+	Description string
+}
+
+type (
+	Characteristics struct {
+		WeaponSkill   Characteristic
+		BalisticSkill Characteristic
+		Strength      Characteristic
+		Toughnes      Characteristic
+		Initative     Characteristic
+		Agility       Characteristic
+		Dexterity     Characteristic
+		Inteligence   Characteristic
+		Willpower     Characteristic
+		Fellowhip     Characteristic
+	}
+
+	Characteristic struct {
+		Basic    int
+		Advances int
+	}
+
+	Skills []Skill
+	Skill  struct {
+		Characteristic Characteristic
+		Basic          int
+		Advances       int
+	}
+
+	Ambitions struct {
+		shortTerm string
+		LongTerm  string
+	}
+
+	Party struct {
+		Name      string
+		Ambitions Ambitions
+		Members   []Caracter
+	}
+
+	Movment struct {
+		Movment int
+		Walk    int
+		Run     int
+	}
+
+	Career struct {
+		Career     string
+		CareerTier int // between 1 and 4
+		CareerPath string
+	}
+
+	Status struct {
+		Tier  StatusTier
+		Level int
+	}
+
+	Description struct {
+		Age         int
+		Height      string
+		Hair        string
+		Eyes        string
+		Description string
+		Psychology  string
+	}
+
+	WornArmour []Armour
+	Armour     struct {
+		Name         string
+		Location     Location
+		Encumbrance  int
+		ArmourPoints int
+		Qualities    []Quality
+		IsWorn       bool
+	}
+
+	Quality struct {
+		Name        string
+		Description string
+		IsFlaw      bool
+	}
+
+	Trapping struct {
+		Name        string
+		Encumbrance int
+		Qualities   []Quality
+		IsWorn      bool
+
+		IsStorage bool
+		StoreEnc  int
+		FilledEnc int
+		Trappings []Trapping
+	}
+
+	Encumbrance struct {
+		Weapons   int
+		Armour    int
+		Trappings int
+		Max       int
+		Total     int
+		Penalty   string
+	}
+
+	// conversion 1GC = 20SS = 240D
+	Welth struct {
+		BrassPenny     int
+		SilverShilling int
+		GoldCrown      int
+	}
+
+	Wounds struct {
+		Max     int
+		Current int
+	}
+
+	Weapon struct {
+		Name        string
+		Group       WeaponGroup
+		Encumbrance int
+		Range_Reach int
+		Damage      string
+		Qualities   Quality
+		IsWorn      bool
+	}
+
+	Prayer struct {
+		Id       uuid.UUID
+		Name     string
+		Range    string
+		Target   string
+		Duration string
+		Effect   string
+	}
+
+	Spell struct {
+		Id            uuid.UUID
+		Name          string
+		CastingNumber int
+		Range         string
+		Target        string
+		Duration      string
+		Effect        string
+	}
+
+	Psychology []string
+
+	Corruption struct {
+		Treshold  int
+		Current   int
+		Mutations []string
+	}
+
+	Points struct {
+		Fate       FPoint
+		Resilience RPoint
+		Exp        ExpPoints
+	}
+
+	FPoint struct {
+		FateMax        int
+		FateCurrent    int
+		FortuneMax     int
+		FortuneCurrent int
+	}
+	RPoint struct {
+		ResilienceMax      int
+		ResilienceMCurrent int
+		ResolveMax         int
+		ResolveCurrent     int
+		Motive             string
+	}
+	ExpPoints struct {
+		Current int
+		Spent   int
+		Total   int
+	}
+)
